e6578d934c1fa09b320b6e8c4a4627180e8e7647
Sooraj R <soorajr0000047@gmail.com>
2026-03-24 01:48:00 +0530

    Fix: scroll-reveal and asset paths for deployment

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }, {
-        threshold: 0.1
+        threshold: 0,
+        rootMargin: '0px 0px -50px 0px'
     });
 
     revealElements.forEach(el => revealObserver.observe(el));
